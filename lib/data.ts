fd94e58a2b9e7cdb2d173ff4686f9df7f3e7a06d
earielle <demix3991@gmail.com>
2026-08-05 19:04:55 +0700

    upd

--- lib/data.ts
+++ lib/data.ts
@@ -41,13 +41,13 @@ export type CommentPhoto = {
 };
 
 export const COMMENT_PHOTOS: CommentPhoto[] = [
-  { src: "/photos/comment-1.jpg", top: "5%", left: "5%", width: "150px", rotate: -8 },
-  { src: "/photos/comment-2.jpg", top: "8%", left: "70%", width: "130px", rotate: 11 },
-  { src: "/photos/comment-3.jpg", top: "34%", left: "2%", width: "170px", rotate: 6 },
-  { src: "/photos/comment-4.jpg", top: "40%", left: "76%", width: "150px", rotate: -12 },
-  { src: "/photos/comment-5.jpg", top: "64%", left: "6%", width: "140px", rotate: 12 },
-  { src: "/photos/comment-6.jpg", top: "68%", left: "68%", width: "160px", rotate: -6 },
-  { src: "/photos/comment-7.jpg", top: "86%", left: "36%", width: "130px", rotate: 5 },
+  { src: "/photos/comment-1.jpg", top: "5%", left: "5%", width: "182px", rotate: -8 },
+  { src: "/photos/comment-2.jpg", top: "8%", left: "70%", width: "202px", rotate: 11 },
+  { src: "/photos/comment-3.jpg", top: "43%", left: "2%", width: "206px", rotate: 6 },
+  { src: "/photos/comment-4.jpg", top: "26%", left: "76%", width: "192px", rotate: -12 },
+  { src: "/photos/comment-5.jpg", top: "53%", left: "6%", width: "174px", rotate: 12 },
+  { src: "/photos/comment-6.jpg", top: "68%", left: "68%", width: "198px", rotate: -6 },
+  { src: "/photos/comment-7.jpg", top: "91%", left: "36%", width: "189px", rotate: 5 },
 ];
 // -------------------------
 
